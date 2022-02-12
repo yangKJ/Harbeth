83e9079eab315e89fe316d2275e2289fc407482b
77。 <393103982@qq.com>
2022-02-12 17:49:20 +0800

    Remove opencv module

--- OpenCV/Pods/Target Support Files/KJCategories/KJCategories-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KJCategories : NSObject
-@end
-@implementation PodsDummy_KJCategories
-@end
--- OpenCV/Pods/Target Support Files/Pods-OpenCV/Pods-OpenCV-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenCV : NSObject
-@end
-@implementation PodsDummy_Pods_OpenCV
-@end
