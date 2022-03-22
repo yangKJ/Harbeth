0a34d5698f870f5e5386db7b5848ef059cdcaa1a
Zz <393103982@qq.com>
2022-03-22 17:14:39 +0800

    Add package

---
+++ Tests/HarbethTests/HarbethTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+
+final class HarbethTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(Harbeth().text, "Hello, World!")
+    }
+}
