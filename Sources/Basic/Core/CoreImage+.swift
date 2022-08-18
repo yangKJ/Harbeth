3f376fc6ca78b404060855063e2b22af01360c07
Condy <yangkj310@gmail.com>
2022-08-18 11:13:00 +0800

    CIFilter originated image to display in IB or simulator with fix #6

--- Sources/Basic/Core/CoreImage+.swift
+++ Sources/Basic/Core/CoreImage+.swift
@@ -29,7 +29,18 @@ internal struct coreimage_ {
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
             return nil
         }
-        let resultImage = C7Image(cgImage: cgImage)
-        return resultImage.mt.toTexture()
+        var image: C7Image
+        
+        #if TARGET_INTERFACE_BUILDER
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return nil
+        }
+        image = C7Image(cgImage: cgImage)
+        #else
+        image = C7Image(ciImage: outputImage)
+        #endif
+        
+        return image.mt.toTexture()
     }
 }
