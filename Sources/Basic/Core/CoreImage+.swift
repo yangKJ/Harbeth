82784939570cfee770c0118b2a346ac6762953b4
Condy <yangkj310@gmail.com>
2022-09-26 17:22:41 +0800

    Fixed invalid CoreImage display

--- Sources/Basic/Core/CoreImage+.swift
+++ Sources/Basic/Core/CoreImage+.swift
@@ -29,18 +29,7 @@ internal struct coreimage_ {
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
             return nil
         }
-        var image: C7Image
-        
-        #if TARGET_INTERFACE_BUILDER
-        let context = CIContext()
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
-            return nil
-        }
-        image = C7Image(cgImage: cgImage)
-        #else
-        image = C7Image(ciImage: outputImage)
-        #endif
-        
-        return image.mt.toTexture()
+        let resultImage = C7Image(cgImage: cgImage)
+        return resultImage.mt.toTexture()
     }
 }
