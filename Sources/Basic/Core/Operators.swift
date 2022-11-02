3095cbebbac73e6439a4794a5881dcf257c21557
Condy <yangkj310@gmail.com>
2022-11-02 17:03:51 +0800

    Add some operator and update outoutable

--- Sources/Basic/Core/Operators.swift
+++ Sources/Basic/Core/Operators.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreVideo
 import MetalKit
 
 precedencegroup AppendPrecedence {
@@ -16,23 +17,43 @@ precedencegroup AppendPrecedence {
 infix operator ->> : AppendPrecedence
 
 @discardableResult
-public func ->> (left: MTLTexture, right: C7FilterProtocol) -> MTLTexture {
-    if let destTexture = try? Processed.IO(inTexture: left, filter: right) {
-        return destTexture
-    }
+public func ->> (left: C7Collector, right: C7FilterProtocol) -> C7Collector {
+    left.filters.append(right)
     return left
+}
+
+@discardableResult
+public func ->> (left: MTLTexture, right: C7FilterProtocol) -> MTLTexture {
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
 }
 
 @discardableResult
 public func ->> (left: C7Image, right: C7FilterProtocol) -> C7Image {
-    if let image: C7Image = try? left.make(filter: right) {
-        return image
-    }
-    return left
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
 }
 
 @discardableResult
-public func ->> (left: C7Collector, right: C7FilterProtocol) -> C7Collector {
-    left.filters.append(right)
-    return left
+public func ->> (left: CGImage, right: C7FilterProtocol) -> CGImage {
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
+}
+
+@discardableResult
+public func ->> (left: CIImage, right: C7FilterProtocol) -> CIImage {
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
+}
+
+@discardableResult
+public func ->> (left: CMSampleBuffer, right: C7FilterProtocol) -> CMSampleBuffer {
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
+}
+
+@discardableResult
+public func ->> (left: CVPixelBuffer, right: C7FilterProtocol) -> CVPixelBuffer {
+    let dest = AnyDest.init(element: left, filters: [right])
+    return (try? dest.output()) ?? left
 }
